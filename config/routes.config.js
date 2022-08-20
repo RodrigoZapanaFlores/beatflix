d45bf73a749dc634d3cbb18881f94eef9e4b3818
RodrigoZapanaFlores <rodrigozapanaflores@gmail.com>
2022-08-20 16:42:56 +0200

    codigo corregido

--- config/routes.config.js
+++ config/routes.config.js
@@ -21,13 +21,7 @@ router.post('/register', auth.doRegister);
 router.get('/login', auth.login);
 router.post('/login', auth.doLogin);
 
-router.post('/logout', (req, res, next) => {
-    req.session.destroy(err => {
-      if (err) next(err);
-      res.redirect('/');
-    });
-  });
-
+router.post('/logout', auth.logout)
 router.get('/users/:id', secure.isAuthenticated, users.userDetail);
 router.get("/users/:id/confirm", users.confirm);
 
